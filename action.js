df46aa89fb60403086bc4d44e44c54cd633c2cda
Benjamin Pahl <8275915+DasBen@users.noreply.github.com>
2023-07-20 10:29:42 +0200

    Init

--- action.js
+++ action.js
@@ -8,21 +8,21 @@ async function run() {
     try {
         // Get the GitHub token input
         const token = core.getInput('github-token');
-        console.log(`GitHub Token used`);
+        core.debug(`GitHub Token: ${token}`);
 
         // Get the required reviewers input
         const requiredReviewersInput = core.getInput('required-reviewers');
         const requiredReviewers = requiredReviewersInput.split(',').map((reviewer) => reviewer.trim());
-        console.log(`Required Reviewers: ${requiredReviewers.join(', ')}`);
+        core.debug(`Required Reviewers: ${requiredReviewers.join(', ')}`);
 
         // Get the required labels input
         const labelsInput = core.getInput('required-labels');
         const requiredLabels = labelsInput.split(',').map((label) => label.trim());
-        console.log(`Required Labels: ${requiredLabels.join(', ')}`);
+        core.debug(`Required Labels: ${requiredLabels.join(', ')}`);
 
         // Get the comment message input or use a default message
         const commentMessage = core.getInput('comment-message') || 'Error: Pull request requires at least one of the following reviewers';
-        console.log(`Comment Message: ${commentMessage}`);
+        core.debug(`Comment Message: ${commentMessage}`);
 
         // Get the Octokit client using the provided token
         const octokit = github.getOctokit(token);
@@ -33,15 +33,15 @@ async function run() {
         // Fetch the pull request details
         const pullRequest = await octokit.pulls.get({ owner, repo, pull_number: number });
         const labels = pullRequest.data.labels.map((label) => label.name);
-        console.log(`Pull Request Labels: ${labels.join(', ')}`);
+        core.debug(`Pull Request Labels: ${labels.join(', ')}`);
 
         // Check if the pull request has the required labels
         const hasRequiredLabels = requiredLabels.every((requiredLabel) => labels.includes(requiredLabel));
-        console.log(`Has Required Labels: ${hasRequiredLabels}`);
+        core.debug(`Has Required Labels: ${hasRequiredLabels}`);
 
         if (hasRequiredLabels) {
             const reviewers = pullRequest.data.requested_reviewers.map((reviewer) => reviewer.login);
-            console.log(`Requested Reviewers: ${reviewers.join(', ')}`);
+            core.debug(`Requested Reviewers: ${reviewers.join(', ')}`);
 
             let foundReviewer = false;
             for (const requiredReviewer of requiredReviewers) {
@@ -50,12 +50,12 @@ async function run() {
                     break;
                 }
             }
-            console.log(`Found Reviewer: ${foundReviewer}`);
+            core.debug(`Found Reviewer: ${foundReviewer}`);
 
             if (!foundReviewer) {
                 const missingReviewersList = requiredReviewers.join(', ');
                 const comment = `${commentMessage}: ${missingReviewersList}`;
-                console.log(`Comment: ${comment}`);
+                core.debug(`Comment: ${comment}`);
 
                 // Check if the comment already exists
                 const comments = await octokit.issues.listComments({ owner, repo, issue_number: number });
@@ -63,11 +63,11 @@ async function run() {
 
                 if (!existingComment) {
                     // Create a new comment
-                    console.log('Creating a new comment...');
+                    core.debug('Creating a new comment...');
                     await octokit.issues.createComment({ owner, repo, issue_number: number, body: comment });
                 } else {
                     // Update the existing comment
-                    console.log('Updating the existing comment...');
+                    core.debug('Updating the existing comment...');
                     await octokit.issues.updateComment({ owner, repo, comment_id: existingComment.id, body: comment });
                 }
 
@@ -79,15 +79,12 @@ async function run() {
 
                 if (existingComment) {
                     // Delete the existing comment
-                    console.log('Deleting the existing comment...');
+                    core.debug('Deleting the existing comment...');
                     await octokit.issues.deleteComment({ owner, repo, comment_id: existingComment.id });
                 }
             }
         }
     } catch (error) {
-        // Log the error message
-        console.error(error.message);
-
         // Set the action as failed
         core.setFailed(error.message);
     }
